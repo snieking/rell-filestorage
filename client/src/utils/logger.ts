e920cfbce5e4ddac82c6580c0a9ceaf0f3c31dc9
Viktor Plane <viktorplane.sonie@gmail.com>
2020-01-01 00:45:30 +0100

    Switch to info logs in production builds

--- client/src/utils/logger.ts
+++ client/src/utils/logger.ts
@@ -1,18 +1,17 @@
 import { createLogger, format, transports } from 'winston';
 
-const level = process.env.REACT_APP_FS_LOG_LEVEL;
-
-const { combine, timestamp, colorize, printf, splat } = format;
+const { combine, timestamp, colorize, printf, splat, label} = format;
 const logger = createLogger({
   format: combine(
     timestamp(),
     colorize(),
     splat(),
-    printf(log => `${log.timestamp} [${log.level}]: ${log.message}`)
+    label({ label: "fs-client" }),
+    printf(log => `${log.timestamp} [${log.level} - ${log.label}]: ${log.message}`)
   ),
   transports: [
     new transports.Console({
-      level: level != undefined ? level : "info"
+      level: process.env.NODE_ENV === "production" ? "info" : "debug"
     })
   ],
   exitOnError: false,
--- test/logger.ts
+++ test/logger.ts
@@ -1,18 +1,17 @@
 import { createLogger, format, transports } from 'winston';
 
-const level = process.env.REACT_APP_TEST_LOG_LEVEL;
-
-const { combine, timestamp, colorize, printf, splat } = format;
+const { combine, timestamp, colorize, printf, splat, label } = format;
 const logger = createLogger({
   format: combine(
     timestamp(),
     colorize(),
     splat(),
+    label({ label: "test" }),
     printf(log => `${log.timestamp} [${log.level}]: ${log.message}`)
   ),
   transports: [
     new transports.Console({
-      level: level != undefined ? level : "debug"
+      level: process.env.NODE_ENV === "production" ? "info" : "debug"
     })
   ],
   exitOnError: false,
